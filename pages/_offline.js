54c18ead7f91752723449277d06d9e3ad7ea1bf8
Максим Лапенков <55906929+MaxLapenkov@users.noreply.github.com>
2023-01-03 21:55:13 +0400

    fin

---
+++ pages/_offline.js
@@ -0,0 +1,9 @@
+import { MainLayout } from "../components/MainLayout";
+
+export default function Offline() {
+  return (
+    <MainLayout>
+      <h1>К сожалению интернет не работает</h1>
+    </MainLayout>
+  );
+}
